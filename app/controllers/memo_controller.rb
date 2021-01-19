1d592aa634aec4ea1080118329f1967cce52ed59
rinatakeuchi-10 <takeuchirina.5@gmail.com>
2021-01-19 16:55:40 +0900

    [Add] memo_app

---
+++ app/controllers/memo_controller.rb
@@ -0,0 +1,20 @@
+ class MemoController < ApplicationController
+   
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  private
+    def post_params
+    params.premit(:content)
+  end
+
+end
